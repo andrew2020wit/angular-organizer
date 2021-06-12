89ab74ee925d3a0626593b5fbfc015855770998e
andrew2020wit <andrew2020wit@gmail.com>
2021-06-12 18:46:12 +0300

    add localStorage

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+
+const localStorageTaskKey = 'localStorageTaskKey';
 
 export interface ITask {
   id?: number;
@@ -7,6 +9,11 @@ export interface ITask {
   description?: string;
   timestamp: number;
   category?: string;
+}
+
+interface ITasksState {
+  currentIdCount: number;
+  tasks: ITask[];
 }
 
 @Injectable({
@@ -18,7 +25,42 @@ export class TasksService {
   public tasks: Record<number, ITask> = {};
   private currentIdCount = 1;
 
-  constructor() {}
+  constructor() {
+    this.loadTasksFromLocalStorage();
+    this.tasksIsChanged$.subscribe((tasksIsChanged) => {
+      if (!tasksIsChanged) {
+        return;
+      }
+      this.saveTaskToLocalStorage();
+    });
+  }
+
+  loadTasksFromLocalStorage() {
+    const tasksStateString = localStorage.getItem(localStorageTaskKey);
+    if (tasksStateString) {
+      const tasksState: ITasksState = JSON.parse(tasksStateString);
+      if (!tasksState.currentIdCount || !tasksState.tasks.length) {
+        return;
+      }
+      this.currentIdCount = tasksState.currentIdCount;
+      this.tasks = {};
+      tasksState.tasks.forEach((task) => {
+        if (!task.id) {
+          return;
+        }
+        this.tasks[task.id] = task;
+      });
+      this.tasksIsChanged$.next(true);
+    }
+  }
+
+  private saveTaskToLocalStorage() {
+    const tasksState: ITasksState = {
+      currentIdCount: this.currentIdCount,
+      tasks: this.getTaskArray(),
+    };
+    localStorage.setItem(localStorageTaskKey, JSON.stringify(tasksState));
+  }
 
   public getTaskCategories() {
     return this.taskCategories;
@@ -27,12 +69,12 @@ export class TasksService {
   public getTaskArray(): ITask[] {
     const res: ITask[] = [];
     if (!this.tasks) {
-      return []
+      return [];
     }
     for (let key in this.tasks) {
       res.push(this.tasks[key]);
     }
-    return res
+    return res;
   }
 
   public editTask(task: ITask) {
@@ -41,6 +83,6 @@ export class TasksService {
       this.currentIdCount++;
     }
     this.tasks[task.id] = task;
-    this.tasksIsChanged$.next(true)
+    this.tasksIsChanged$.next(true);
   }
 }
